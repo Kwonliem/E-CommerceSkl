03449a64c241d512c97aa10573f2b3d59acfda87
liem <liemyoesuf@gmail.com>
2024-05-17 19:19:22 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories');
+Route::get('/details/{id}', [App\Http\Controllers\DetailController::class, 'index'])->name('deta');
+
+
+
+Auth::routes();
